eab1b62db3ae8da2064fead727329c93f1eac144
anon <anon@anon-VirtualBox.(none)>
2012-11-06 10:58:34 -0700

    test config

--- app.py
+++ app.py
@@ -2,3 +2,10 @@ import sys
 sys.path.append('libs/')
 import sqlite3
 from flask import *
+
+# configuration
+DATABASE = '/tmp/flaskr.db'
+DEBUG = True
+SECRET_KEY = 'development key'
+USERNAME = 'admin'
+PASSWORD = 'default'
