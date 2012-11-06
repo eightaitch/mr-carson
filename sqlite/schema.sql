aca8d68a56331f0abc609d37fadf3762e3e7adfb
anon <anon@anon-VirtualBox.(none)>
2012-11-06 11:10:12 -0700

    sqlite db

--- sqlite/schema.sql
+++ sqlite/schema.sql
@@ -2,7 +2,7 @@ drop table if exists server;
 create table server (
   id integer primary key autoincrement,
   host string not null,
-  port string not null
-  username string not null
+  port string not null,
+  username string not null,
   password string not null
 );
