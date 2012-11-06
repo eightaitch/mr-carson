2e1cbd573752f37675b0858120aefaf4ec474946
anon <anon@anon-VirtualBox.(none)>
2012-11-06 15:37:30 -0700

    working select and value form fill for server settings

--- app.py
+++ app.py
@@ -31,11 +31,12 @@ def server():
     server_config = g.db.execute('select * from server').fetchone()
     if server_config != None:
         # server entry exists
-        values = [dict(host=server_config[0],
-                       port=server_config[1],
-                       username=server_config[2],
-                       password=server_config[3])]
-        return render_template('server.html', server=values)
+        #values = dict('host'=server_config[0])
+        server = dict(host=server_config[1],
+                 port=server_config[2],
+                 username=server_config[3],
+                 password=server_config[4])
+        return render_template('server.html', server=server)
     else:
         #server needs configuration    
         return render_template('server.html')
